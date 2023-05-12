3988ed1cab8313d249abde14dc43ccd079990aa7
pankajjarial360 <87800233+pankajjarial360@users.noreply.github.com>
2023-05-12 12:54:55 +0530

    Update main.py
    added GMT, MDT and UTC time zone standards for the conversion

--- TZC/main.py
+++ TZC/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from datetime import datetime
 import pytz
 
+def format_datetime(dt, tz):
+    formatted_dt = dt.astimezone(pytz.timezone(tz))
+    return formatted_dt.strftime("%Y-%m-%d %I:%M:%S %p")
+
+
 app = FastAPI()
 
 @app.get("/convert_timezone")
@@ -18,6 +23,9 @@ def convert_timezone(from_country: str, from_timezone: str, to_country: str, to_
 
         # Format the converted datetime
         converted_time = converted_dt.strftime("%Y-%m-%d %I:%M:%S %p")
+        converted_time_gmt = format_datetime(converted_dt, 'GMT')
+        converted_time_mdt = format_datetime(converted_dt, 'US/Mountain')
+        converted_time_utc = format_datetime(converted_dt, 'UTC')
 
         return {
             "from_country": from_country,
@@ -25,10 +33,13 @@ def convert_timezone(from_country: str, from_timezone: str, to_country: str, to_
             "to_country": to_country,
             "to_timezone": to_timezone,
             "datetime": datetime_str,
-            "converted_time": converted_time
+            "converted_time": converted_time,
+            "converted_time_gmt": converted_time_gmt,
+            "converted_time_mdt": converted_time_mdt,
+            "converted_time_utc": converted_time_utc
         }
-    except (pytz.UnknownTimeZoneError, ValueError):
-        return {"error": "Invalid time zone or datetime format provided."}
+    except ValueError:
+        return {"error": "Invalid datetime format provided."}
 
-# http://127.0.0.1:8000/convert_timezone?from_country=IN&from_timezone=Asia/Kolkata&to_country=PK&to_timezone=Asia/Karachi&datetime_str=2023-05-11T12:00:00
-
+# Running on browser here is the url, You can change the country and timezone ......!!!
+# http://127.0.0.1:8000/convert_timezone?from_country=IN&from_timezone=Asia/Kolkata&to_country=IN&to_timezone=Asia/Kolkata&datetime_str=2023-05-11T12:00:00
